38ddbe06b3f4bce2c9ae7d14f10142aa278e03dd
yoshelinn <gabrielayoshelin@gmail.com>
2023-09-25 14:24:12 +0700

    Initial commit

--- main/urls.py
+++ main/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     ]
+
